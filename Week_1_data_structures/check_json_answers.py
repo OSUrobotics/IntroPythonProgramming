21d9c220cfd93727e9184224e009e0ac78ddd18c
Cindy Grimm <cindy.grimm@oregonstate.edu>
2024-01-17 12:19:03 -0800

    Small spelling errors, someone's added meta data and outputs cell

--- Week_1_data_structures/check_json_answers.py
+++ Week_1_data_structures/check_json_answers.py
@@ -4,29 +4,34 @@
 import numpy as np
 import json as json
 
-
-def compare_files(fname_1, fname_2):
-    student = fname_1
-    if isinstance(fname_1, str):
-        with open("Data/" + fname_1, "r") as fp:
+# Compare dictionaries or data to files
+def compare_files(student_answer, soln_file_name):
+    # Student is always the first argument
+    student = student_answer
+    # If it's a file name, then load that file
+    if isinstance(student_answer, str):
+        with open("Data/" + student_answer, "r") as fp:
             student = json.load(fp)
 
-    check = fname_2
-    if isinstance(fname_2, str):
-        with open("Data/" + fname_2, "r") as fp:
-            check = json.load(fp)
+    # Second filename is always the solution, stored in a file or a dictionary. Load it if it is a file.
+    soln = soln_file_name
+    if isinstance(soln_file_name, str):
+        with open("Data/" + soln_file_name, "r") as fp:
+            soln = json.load(fp)
 
-    if isinstance(check, dict):
-        for k, v in check.items():
+    # If soln is a dictionary, then do the first if statement
+    if isinstance(soln, dict):
+        # Loop through all elements of the dictionary
+        for k, v in soln.items():
             try:
                 if k == "Data channels":
                     # The data channels list
                     for i, d in enumerate(student[k]):
-                        if not d == v[i]:
-                            print(f"miss-match {d} {v[i]}")
+                        if d != v[i]:
+                            print(f"miss-match value {d} {v[i]}")
                             return False
                 else:
-                    if not v == student[k]:
+                    if v != student[k]:
                         print(f"Miss-match key-item {k} {v} {student[k]}")
                         return False
             except KeyError:
@@ -34,18 +39,22 @@ def compare_files(fname_1, fname_2):
                 return False
         return True
     else:
-        if len(check) != len(student):
-            print(f"Mismatched number of elements")            
+        # soln is a list.
+        if len(soln) != len(student):   # Check lists are same length
+            print(f"Mismatched number of elements, solution {len(soln)} yours {len(student)}")            
             return False
 
-        for ce, cs in zip(check, student):
+        # For each item in each list
+        for ce, cs in zip(soln, student):
+            # Each item is a dictionary - loop through the solution keys
             for k, v in ce.items():
                 try:
-                    if not v == cs[k]:
-                        print(f"Miss-match key-item {k} {v} {cs[k]}")
+                    # Check if the values in the keys are similar
+                    if not np.isclose(v, cs[k], atol=0.01):
+                        print(f"Value for key {k} is {cs[k]}, expected {v}")
                         return False
                 except KeyError:
-                    print(f"Missing key {k}")
+                    print(f"Missing key {k} or not a number {cs[k]}")
                     return False
     return True
         
